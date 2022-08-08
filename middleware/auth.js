b00c0d57b82e766f8a498a347064b07e95729a42
Aimee Sharp <sharpaim23@gmail.com>
2022-08-08 08:25:45 -0500

    Finished StoryBooks App

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const { nextTick } = require("async")
+
+module.exports = {
+  ensureAuth: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    } else {
+      res.redirect('/')
+    }
+  },
+  ensureGuest: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      res.redirect('/dashboard')
+    } else {
+      return next()
+    }
+  },
+}
